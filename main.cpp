29aec778d82020005273eac78e10731b1808df7a
Vladled <vladikzhovnir@ukr.net>
2021-12-13 16:51:46 +0200

    Add file dev branch

--- main.cpp
+++ main.cpp
@@ -6,7 +6,7 @@ int main(int argc, char *argv[ ])
 QApplication app(argc, argv);
  
 QPushButton button("Hello, World!");
-button.resize(200, 60);
+button.resize(200, 70);
 button.show( );
  
 return app.exec( );
